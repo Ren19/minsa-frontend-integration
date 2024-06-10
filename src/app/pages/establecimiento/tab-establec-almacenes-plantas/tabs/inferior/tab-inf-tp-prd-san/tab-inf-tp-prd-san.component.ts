21889b00c793dfd3b26bbcfabc922c8c6dca1eaf
César Samamé <cesar.enrique129@gmail.com>
2024-06-10 13:58:10 -0500

    Tipo Producto diseño

--- src/app/pages/establecimiento/tab-establec-almacenes-plantas/tabs/inferior/tab-inf-tp-prd-san/tab-inf-tp-prd-san.component.ts
+++ src/app/pages/establecimiento/tab-establec-almacenes-plantas/tabs/inferior/tab-inf-tp-prd-san/tab-inf-tp-prd-san.component.ts
@@ -1,4 +1,6 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { PeriodicElement } from 'src/app/pages/representantebusqueda01/representantebusqueda01.component';
 
 @Component({
   selector: 'app-tab-inf-tp-prd-san',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab-inf-tp-prd-san.component.css']
 })
 export class TabInfTpPrdSanComponent {
+
+  displayedTipoProducto: string[] = ['tipoProducto'];
+  dataSourceTipoProducto: any = []
+  dataSourceCopy: any = []
+  selectionPersonal = new SelectionModel<PeriodicElement>(true, []);
 
 }
