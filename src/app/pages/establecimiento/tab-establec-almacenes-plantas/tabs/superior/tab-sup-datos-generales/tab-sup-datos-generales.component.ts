cd3fba3818e8569111438906dfa4ad348b3ba0da
César Samamé <cesar.enrique129@gmail.com>
2024-06-11 13:12:38 -0500

    Datos Generales diseño

--- src/app/pages/establecimiento/tab-establec-almacenes-plantas/tabs/superior/tab-sup-datos-generales/tab-sup-datos-generales.component.ts
+++ src/app/pages/establecimiento/tab-establec-almacenes-plantas/tabs/superior/tab-sup-datos-generales/tab-sup-datos-generales.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MdEstablecDistritoComponent } from 'src/app/pages/establecimiento/modals/md-establec-distrito/md-establec-distrito.component';
+import { ComunService } from 'src/app/services/comun.service';
 
 @Component({
   selector: 'app-tab-sup-datos-generales',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab-sup-datos-generales.component.css']
 })
 export class TabSupDatosGeneralesComponent {
+
+  codigoDepartamento: any = null
+  textoDepartamento: any = null
+  codigoProvincia: any = null
+  textoProvincia: any = null
+  codigoDistrito: any = null
+  textoDistrito: any = null
+  textoDepProDep: any = null
+
+  constructor(
+    public dialog: MatDialog,
+    public comunService: ComunService
+  ) { }
+
+  openModalDistrito() {
+    const dialogRef = this.dialog.open(MdEstablecDistritoComponent, {
+      width:'850px',
+      height:'550px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result != null) {
+        this.codigoDepartamento = result.DPTCODIGO;
+        this.textoDepartamento = result.DPTDESCRIP;
+        this.codigoProvincia = result.PRVCODIGO;
+        this.textoProvincia = result.PRVDESCRIP;
+        this.codigoDistrito = result.DISCODIGO;
+        this.textoDistrito = result.DISDESCRIP;
+        this.textoDepProDep = `${result.DPTDESCRIP} / ${result.PRVDESCRIP} / ${result.DISDESCRIP}`;
+      }
+    });
+  }
 
 }
